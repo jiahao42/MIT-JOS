9fe8b31446db27288969b7c0bd637412032ea811
James <caterpillarous@gmail.com>
2017-02-23 00:07:16 -0800

    add ASM practice

---
+++ PC_Assembly_language/sample1.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: sample1.c
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 23 Feb 2017 12:04:43 AM PST
+ ************************************************************************/
+
+#include<stdio.h>
+int main(void) {
+    int ret_status;
+    ret_status = asm_main();
+    return ret_status;
+}
